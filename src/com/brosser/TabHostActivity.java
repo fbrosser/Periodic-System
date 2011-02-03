87a08dedef8bcc6ec5d0435bc750247b3170e3a8
Pintsize <fb.msngr@gmail.com>
2011-02-03 20:12:30 +0100

    TabHostActivity

---
+++ src/com/brosser/TabHostActivity.java
@@ -0,0 +1,45 @@
+package com.brosser;
+
+import com.brosser.model.ElementTable;
+
+import android.app.TabActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.widget.TabHost;
+
+public class TabHostActivity extends TabActivity {
+	
+	/** Called when the activity is first created. */
+	protected void onCreate(Bundle savedInstanceState) {
+
+		super.onCreate(savedInstanceState);
+		TabHost tabHost = getTabHost();
+		
+		// Inflate the view
+		LayoutInflater.from(this).inflate(R.layout.tabs, tabHost.getTabContentView(), true);
+		
+		// Set up the element table
+		ElementTable elementTable = ElementTable.getInstance(getResources());
+	
+		// Add tabs
+		tabHost.addTab(tabHost.newTabSpec("element_view")
+				.setIndicator("",
+						getResources().getDrawable(R.drawable.atom))
+				.setContent(new Intent(this, ElementActivity.class)
+					.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)));
+
+		tabHost.addTab(tabHost.newTabSpec("table_view")
+				.setIndicator("", 
+						getResources().getDrawable(R.drawable.table))
+				.setContent(new Intent(this, TableActivity.class)
+					.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)));
+		
+		tabHost.addTab(tabHost.newTabSpec("settings_view")
+				.setIndicator("",
+					getResources().getDrawable(R.drawable.android_settings_icon))
+					.setContent(new Intent(this, SettingsActivity.class)
+					.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)));
+	}
+
+}
