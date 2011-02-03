036bdb7b3394c2ea6dedb8a517b8f71866b23876
Pintsize <fb.msngr@gmail.com>
2011-02-03 19:49:54 +0100

    Table Activity

---
+++ src/com/brosser/TableActivity.java
@@ -0,0 +1,58 @@
+package com.brosser;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.view.View.OnClickListener;
+import android.content.pm.ActivityInfo;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import com.brosser.model.ElementTable;
+
+public class TableActivity extends Activity implements OnClickListener {
+	
+	private Button[][] buttons = new Button[18][7];
+    
+	/** Called when the activity is first created. */
+    @Override 
+    public void onCreate(Bundle state) {
+        super.onCreate(state);
+        
+        // Install the view
+        setContentView(R.layout.table);
+        
+        // Populate the button matrix
+    	for(int i=0; i<ElementTable.getGroups(); i++) {
+    		for(int j=0; j<ElementTable.getPeriods(); j++) {
+    			String buttonID = "btn" + i + "a" + j;
+    			int resID = getResources().getIdentifier(buttonID, "id", "com.brosser");
+    			buttons[i][j] = ((Button) findViewById(resID));
+    			buttons[i][j].setOnClickListener(this);
+    		}
+    	}
+    }
+    
+    @Override
+    protected void onResume() {
+    	super.onResume();
+    	// Fix screen orientation to landscape mode
+    	setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+    }
+
+    /** Callback function for the buttons */
+	@Override
+	public void onClick(View src)  {
+
+		for(int i=0; i<ElementTable.getGroups(); i++) {
+			for(int j=0; j<ElementTable.getPeriods(); j++) {
+				if(src == buttons[i][j]) {
+					ElementTable.setActiveElement(ElementTable.getElement(i, j));
+				}
+			}
+		}
+		
+		// Show element view for the selected element
+		Intent intent = new Intent(this, ElementActivity.class);
+		startActivity(intent);
+	}
+}
