ce29ac62c615e219e5e905fe9cadf666cb55722f
Pintsize <fb.msngr@gmail.com>
2011-02-19 00:31:34 +0100

    Implemented SplashActivity

--- src/com/brosser/SplashActivity.java
+++ src/com/brosser/SplashActivity.java
@@ -1,6 +1,7 @@
 package com.brosser;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MotionEvent;
@@ -8,19 +9,26 @@ import android.view.MotionEvent;
 public class SplashActivity extends Activity {
 	
     protected boolean _active = true;
-    protected int _splashTime = 5000;
+    protected int _splashTime = 10000;
+    protected Context packageContext;
     
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.splashscreen);
+        this.packageContext = this;
         
         // Thread for displaying the splash screen
         Thread splashTread = new Thread() {
             
         	@Override
             public void run() {
+        		
+        		// Start database handler
+        		DatabaseHandler databaseHandler = new DatabaseHandler(getResources());
+        		databaseHandler.start();
+        		
         		try {
         			int waited = 0;
                     while(_active && (waited < _splashTime)) {
@@ -34,7 +42,9 @@ public class SplashActivity extends Activity {
                 	} finally {
                 		finish();
                 		// Start the real application
-                		startActivity(new Intent("com.brosser.TabHostActivity"));
+                		//startActivity(new Intent("com.brosser.TabHostActivity"));
+                		//startActivity(new Intent("com.brosser.SettingsActivity"));
+                		startActivity(new Intent(packageContext, TabHostActivity.class));
                 		stop();
                 }
             }
