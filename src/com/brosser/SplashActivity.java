4e2d9ee103085311149f22d6db8361547af44293
Pintsize <fb.msngr@gmail.com>
2011-02-19 17:15:39 +0100

    Splash now waits for database to finish

--- src/com/brosser/SplashActivity.java
+++ src/com/brosser/SplashActivity.java
@@ -1,16 +1,28 @@
 package com.brosser;
 
+import com.brosser.model.Element;
+
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.MotionEvent;
+import android.widget.TextView;
 
 public class SplashActivity extends Activity {
 	
     protected boolean _active = true;
-    protected int _splashTime = 10000;
+    protected int _splashTime = 2000;
     protected Context packageContext;
+    private static TextView displayNumber;
+    
+    private final Runnable mUpdateUITimerTask = new Runnable() {
+        public void run() {
+            displayNumber.setText("Done!");
+        }
+    };
+    private final Handler mHandler = new Handler();
     
     /** Called when the activity is first created. */
     @Override
@@ -18,6 +30,7 @@ public class SplashActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.splashscreen);
         this.packageContext = this;
+        displayNumber = ((TextView)findViewById(R.id.splashNumber));
         
         // Thread for displaying the splash screen
         Thread splashTread = new Thread() {
@@ -35,15 +48,17 @@ public class SplashActivity extends Activity {
                         sleep(100);
                         if(_active) {
                             waited += 100;
+                           databaseHandler.join();
+                            mHandler.postDelayed(mUpdateUITimerTask, 10);
+                            Element.setLanguage(DatabaseHandler.getLanguage(1));
                         }
                     }
                 } catch(InterruptedException e) {    
                 	// Do nothing
                 	} finally {
+                		// Languages
                 		finish();
                 		// Start the real application
-                		//startActivity(new Intent("com.brosser.TabHostActivity"));
-                		//startActivity(new Intent("com.brosser.SettingsActivity"));
                 		startActivity(new Intent(packageContext, TabHostActivity.class));
                 		stop();
                 }
