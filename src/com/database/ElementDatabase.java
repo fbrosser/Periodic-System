ffa6e1810722b572bfb0b9d4d0920039b8f18005
Fredrik Brosser <fb.msngr@gmail.com>
2011-03-05 15:21:56 +0100

    Added ElementDatabase

---
+++ src/com/database/ElementDatabase.java
@@ -0,0 +1,32 @@
+package com.database;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class ElementDatabase extends SQLiteOpenHelper {
+
+	/* The name of the database file on the file system*/
+	private static final String DATABASE_NAME = "PeriodicTable";
+	/* The version of the database that this class understands*/
+	private static final int DATABASE_VERSION = 1;
+	/* The context, needed to load SQL from string resources*/
+	private final Context mContext;
+	
+	public ElementDatabase(Context context) {
+		super(context, DATABASE_NAME, null, DATABASE_VERSION);
+		this.mContext = context;
+	}
+	
+	@Override
+	public void onCreate(SQLiteDatabase database) {
+		
+	}
+
+	@Override
+	public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
